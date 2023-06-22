3d3e318c916b489d9f175983aad20a7633343cc2
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-06-21 22:46:39 -0500

    Add files via upload

---
+++ betterThanAverage.js
@@ -0,0 +1,16 @@
+function betterThanAverage(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    avg = (sum / arr.length)
+    var count = 0
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > avg) {
+            count++
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
